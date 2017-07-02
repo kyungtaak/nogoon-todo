bb5fdc02f7fbace904e4bb4d2612fe74e8f49aba
Kyungtaak Noh <kyungtaak@sk.com>
2017-07-02 11:11:14 +0900

    initial commit

---
+++ todo-server/src/main/java/com/nogoon/apps/ToDoApplication.java
@@ -0,0 +1,14 @@
+package com.nogoon.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by kyungtaak on 2017. 6. 29..
+ */
+@SpringBootApplication
+public class ToDoApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ToDoApplication.class, args);
+  }
+}
